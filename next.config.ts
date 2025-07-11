a0f1e0a2926abe540e66b4f8064e83171d46db2a
Chinmay <chinmaybadwaik9@gmail.com>
2025-07-11 18:01:08 +0530

    project added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "res.cloudinary.com",
+       "assets.bayut.com", 
+       "oss.pixxicrm.com", 
+       "s3-ap-southeast-1.amazonaws.com"
+    ], 
+  },
 };
 
 export default nextConfig;
